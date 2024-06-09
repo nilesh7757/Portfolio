4d72a371ff6b0b0b8dc441110567c09bfa4e6ebe
nilesh7757 <nileshmori7757@gmail.com>
2024-06-09 09:56:36 +0530

    Some Change To Make Responsive

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import './Navbar.css'
 const Navbar = () => {
   return (
     <>
-      <nav className='flex justify-between mt-2 py-3 sticky top-0 w-full'>
-        <div className='text-2xl bg-black text-white size-10 flex justify-center items-center font-bold p-6 rounded-full'>NM</div>
-        <div>
-          <ul className='flex Navbar text-xl gap-4'>
+      <nav className='header flex mt-2 py-3 sticky top-0 w-full'>
+        <div className='text-2xl bg-black text-white size-10  flex justify-center items-center font-bold p-6 rounded-full'>NM</div>
+        <div className=''>
+          {<ul className='navbar md:flex Navbar text-xl gap-4'>
             <li className='hover:font-bold hover:text-blue-500'><Link href = "#">Home</Link></li>
             <li className='hover:font-bold hover:text-blue-500'><Link href = "#">About Me</Link></li>
             <li className='hover:font-bold hover:text-blue-500'><Link href = "#">Skills</Link></li>
             <li className='hover:font-bold hover:text-blue-500'><Link href = "#">Education</Link></li>
             <li className='hover:font-bold hover:text-blue-500'><Link href = "#">Contact Me</Link></li>
-          </ul>
+          </ul>}
         </div>
       </nav>
     </>
--- app/components/TypedText.js
+++ app/components/TypedText.js
@@ -3,6 +3,7 @@
 import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import Typed from 'typed.js';
+import './typedtext.css'
 
 const TypedText = () => {
   const el = useRef(null);
@@ -22,21 +23,21 @@ const TypedText = () => {
 
   return (
     <>
-    <div className='mt-36 px-20 flex flex-col'>
-    <div className='text-5xl'>
+    <div className='lg:mt-36 sm:mt-16 px-20 flex flex-col'>
+    <div className='info lg:text-4xl lg:w-[80%] sm:text-2xl'>
       <h1 className='font-medium'>Hey There,</h1>
-      <h1 className='mt-8 font-medium'>Im Nilesh <span className='text-[#FF2D00]'>Mori</span></h1>
-      <h1 className='mt-8 font-medium'>I am In <span className="text-blue-500" ref={el}></span></h1>
-      <button className='bg-blue-500 mt-8 hover:font-bold hover:bg-blue-700 text-xl px-3 py-1 rounded-lg text-white'>About Me</button>
+      <h1 className='lg:mt-8 sm:mt-5 font-medium'>Im Nilesh <span className='text-[#FF2D00]'>Mori</span></h1>
+      <h1 className='lg:mt-8 sm:mt-5 font-medium'>I am <span className="text-blue-500" ref={el}></span></h1>
+      <button className='bg-blue-500 lg:mt-8 sm:mt-5 hover:font-bold hover:bg-blue-700 lg:text-xl sm:text-xl px-3 py-1 rounded-lg text-white'>About Me</button>
     </div>
-    <div className="img absolute top-32 rounded-full right-0">
-      <Image className='rounded-full' src="./NileshMori.jpg" width={300} height={300} alt="" />
+    <div className="img absolute w-[20%] h-full rounded-full right-0">
+      <Image className='rounded-full' src="./NileshMori.jpg" width={250} height={250} alt="" />
     </div>
-    <div className='contacts flex gap-5 mt-5'>
-      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 items-center p-1 rounded-full object-contain object-center"><Image className="relative top-1" src="./fb.svg"  width="30" height="30" alt=""/></div>
-      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 items-center p-1 rounded-full object-contain object-center"><Image className="" src="./gh.svg"  width="30" height="30" alt=""/></div>
-      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 items-center  rounded-full object-contain object-center"><Image className="" src="./linkdin.svg"  width="28" height="28" alt=""/></div>
-      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 items-center  rounded-full object-contain object-center"><Image className="" src="./X.svg"  width="30" height="30" alt=""/></div>
+    <div className='contacts flex gap-5 md:mt-5'>
+      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 sm:w-8 items-center p-1 rounded-full object-contain object-center"><Image className="relative top-1" src="./fb.svg"  width="25" height="25" alt=""/></div>
+      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 sm:w-8 items-center p-1 rounded-full object-contain object-center"><Image className="" src="./gh.svg"  width="25" height="25" alt=""/></div>
+      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 sm:w-8 items-center  rounded-full object-contain object-center"><Image className="" src="./linkdin.svg"  width="20" height="20" alt=""/></div>
+      <div className="bg-blue-500 hover:bg-blue-600 flex justify-center w-10 sm:w-8 items-center  rounded-full object-contain object-center"><Image className="" src="./X.svg"  width="25" height="25" alt=""/></div>
        {/* <div className="bg-white w-12 rounded-full mt-5"><img className="object-contain object-center fill-blue-500" src="./gh.svg" alt="" /></div>
       <div className="bg-blue-500 hover:bg-blue-600 flex justify-center items-center w-12 rounded-full mt-5 object-contain object-center"><img className="w-9" src="./linkdin.svg" alt="" /></div>
       <div className="bg-blue-500 hover:bg-blue-600 flex justify-center items-center w-12 rounded-full mt-5 object-contain object-center"><img className="w-9" src="./X.svg" alt="" /></div> */}
