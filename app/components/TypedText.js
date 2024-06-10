eb91302cbc123880db7d3772a407d16fc985efa6
nilesh7757 <nileshmori7757@gmail.com>
2024-06-10 12:26:05 +0530

    Icon Link Added

--- app/components/TypedText.js
+++ app/components/TypedText.js
@@ -1,6 +1,7 @@
 // components/TypedText.js
 "use client"
 import { useEffect, useRef } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import HorizontalNav from './HorizontalNav';
 import { FaGithub } from "react-icons/fa";
@@ -27,17 +28,17 @@ const TypedText = ()=>{
           height={40}
         />
         </div>
-      <div className="text-3xl items-center text-center w-full lg:items-start  top-[300px] relative lg:left-20  md:text-5xl {inter.className} font-bold flex flex-col lg:top-16  p-10 gap-y-5 sm:gap-y-12">
+      <div className="text-3xl items-center text-center w-full lg:items-start  top-[200px] relative lg:left-20  md:text-5xl {inter.className} font-bold flex flex-col lg:top-16  p-10 gap-y-5 sm:gap-y-12">
         <h1 className='text-purple-700'>Hey</h1> 
         <h1>Nilesh <span className='text-blue-500'>Mori</span></h1>
         <h1><span className='text-red-500'>Fullstack</span> Webdevloper</h1>
         <button className='bg-blue-500 hover:bg-blue-800 text-xl w-fit px-5 py-2 rounded-lg text-white'>About Me</button>
       <div className="contacts flex gap-x-3">
-          <div className="github"><FaGithub fill='#3b82f6' /></div>
-          <div className="github"><FaFacebook fill='#3b82f6'/></div>
-          <div className="github rounded-full bg-blue-500 w-[1em] h-[1em] flex items-center justify-center"><FaLinkedinIn fill='white' size={"0.7em"} color='white' /></div>
-          <div className="github rounded-full bg-blue-500 flex justify-center items-center w-[1em] h-[1em]"><FaInstagram fill='white' color='white' size={"0.8em"} /></div>
-          <div className="github rounded-full bg-blue-500 flex justify-center items-center w-[1em] h-[1em]"><FaXTwitter fill='white' color='white' size={"0.7em"} /></div>
+          <div><Link href="https://github.com/nilesh7757"><FaGithub fill='#3b82f6' /></Link></div>
+          <div><Link href="https://www.facebook.com/akash.mori.792/"><FaFacebook fill='#3b82f6'/></Link></div>
+          <div className="rounded-full bg-blue-500 w-[1em] h-[1em] flex items-center justify-center"><Link href = "https://www.linkedin.com/in/nilesh-mori-7757n/"><FaLinkedinIn fill='white' size={"0.7em"}  /></Link></div>
+          <div className="rounded-full bg-blue-500 flex justify-center items-center w-[1em] h-[1em]"><Link href= "https://www.instagram.com/nilesh_mori_7757/"><FaInstagram fill='white'  size={"0.8em"} /></Link></div>
+          <div className="rounded-full bg-blue-500 flex justify-center items-center w-[1em] h-[1em]"><Link href="https://x.com/Programmer7757?t=VP9FEJwB8i_RPSXHwS7chw&s=08"><FaXTwitter fill='white'  size={"0.7em"} /></Link></div>
       </div>
       </div>
         
